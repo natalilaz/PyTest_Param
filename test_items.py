dd9f74771c96c4e3a593d22956e74f22c340ddbe
Natali <aventur@yandex.ru>
2020-10-22 19:38:34 +0300

    тест, который проверяет, что страница товара на сайте содержит кнопку добавления в корзину

---
+++ test_items.py
@@ -0,0 +1,8 @@
+#import time
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+def test_user_should_see_btn_add_to_basket(browser):
+    browser.get(link)
+    #time.sleep(30)
+    button = browser.find_element_by_css_selector("button.btn-add-to-basket")
+    assert button, print("Кнопка не найдена")
